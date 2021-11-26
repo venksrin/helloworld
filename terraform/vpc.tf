77a340085d24127b3568fbffba096ad6620fc3db
venksrin <venkys_us@yahoo.com>
2021-11-25 21:27:57 -0800

    Add files via upload

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,47 +1,20 @@
-variable "region" {
-  default     = "us-east-1"
-  description = "AWS region"
-}
-
-provider "aws" {
-  region = var.region
-}
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  cluster_name = "you-cluster"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.2.0"
+  version = "2.59.0"
 
-  name                 = "you-vpc"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  name = "you-vpc"
+  cidr = "10.1.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
 
   tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
+    "Name"                                      = "you-terraform-eks"
+    "kubernetes.io/cluster/${var.cluster-name}" = "shared"
   }
 }
+
