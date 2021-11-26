06bcdfbccafde44b2c7304f189603c0f9b9bc32d
venksrin <venkys_us@yahoo.com>
2021-11-25 21:20:38 -0800

    Add files via upload

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,33 +1,15 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "you_cluster"
-  cluster_version = "1.20"
-  subnets         = module.vpc.private_subnets
+resource "aws_eks_cluster" "you" {
+  name     = var.cluster-name
+  role_arn = aws_iam_role.you-cluster.arn
 
-  tags = {
-    Environment = "you"
+  vpc_config {
+    security_group_ids = [aws_security_group.you-cluster-sg.id]
+    subnet_ids = module.vpc.public_subnets
   }
 
-  vpc_id = module.vpc.vpc_id
-
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
-
-  worker_groups = [
-    {
-      name                          = "hello-world-app-group-1"
-      instance_type                 = "t2.micro"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.helloworld_access.id,aws_security_group.worker_group_mgmt_one.id]
-    },
+  depends_on = [
+    aws_iam_role_policy_attachment.you-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.you-cluster-AmazonEKSServicePolicy,
   ]
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
