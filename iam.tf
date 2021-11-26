e28f17f5e6ed8191d7e8b8e01a9748644c1286c7
venksrin <venkys_us@yahoo.com>
2021-11-25 21:21:22 -0800

    Add files via upload

---
+++ iam.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_role" "you-cluster" {
+  name = "terraform-eks-you-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+
+}
+
+resource "aws_iam_role_policy_attachment" "you-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role = aws_iam_role.you-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "you-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role = aws_iam_role.you-cluster.name
+}
+
+# If no loadbalancer was ever created in this region, then this following role is necessary
+resource "aws_iam_role_policy" "you-cluster-service-linked-role" {
+  name = "service-linked-role"
+  role = aws_iam_role.you-cluster.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "arn:aws:iam::*:role/aws-service-role/*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:DescribeAccountAttributes"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+}
+
